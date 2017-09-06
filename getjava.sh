715521b45bf606aa1a50e88f82597b5f6b18db6f
Anthony Roberts <anthony.roberts@monitise.com>
2017-09-06 14:41:42 +0100

    Delete getjava.sh

--- getjava.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget --no-cookies \
---no-check-certificate \
---header "Cookie: oraclelicense=accept-securebackup-cookie" \
-"http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-linux-x64.tar.gz?AuthParam=1504702184_7a14fcd76af8b8f8db6dc282e94b80d6" \
--O jdk-8-linux-x64.tar.gz
