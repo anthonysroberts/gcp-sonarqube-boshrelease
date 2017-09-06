db65eba1ddc97a103ad02ed4f043c0c7fcafcfc3
Anthony Roberts <anthony.roberts@monitise.com>
2017-09-06 13:45:17 +0100

    Update getjava.sh

--- getjava.sh
+++ getjava.sh
@@ -4,5 +4,5 @@ wget --no-cookies \
 --no-check-certificate \
 --header "Cookie: oraclelicense=accept-securebackup-cookie" \
 # "http://download.oracle.com/otn-pub/java/jdk/8-b132/jdk-8-linux-x64.tar.gz" \
-"http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/server-jre-8u131-linux-x64.tar.gz" \
+"http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-linux-x64.tar.gz?AuthParam=1504701947_6760c74857c64ab7543f3693a2b7464a" \
 -O jdk-8-linux-x64.tar.gz
